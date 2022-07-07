9d53c40a4af2490d4009f4ddc14cef5a699374de
elzari <86085151+elzari@users.noreply.github.com>
2022-07-07 05:24:09 +0200

    Add files via upload

---
+++ Airbrush.js
@@ -0,0 +1,7 @@
+var obj = JSON.parse($response.body);
+obj = {
+
+"code":0,"data":{"activityInfos":[{"abTesting":0,"activityName":"defaultSKU","activityType":0,"description":"","endTime":1712309220,"id":128,"materialInfos":[],"periodicFreeFeatureInfos":[],"productInfos":[{"activityProduct":{},"defaultProduct":{"productId":"com.magicv.AirBrush.sub.allAccess.1year.newgeo10.fullPrice","promotionalOfferId":""},"isSubscription":true,"productType":"year"},{"activityProduct":{},"defaultProduct":{"productId":"com.magicv.AirBrush.sub.allAccess.1month.newgeo10.fullPrice","promotionalOfferId":""},"isSubscription":true,"productType":"month"},{"activityProduct":{},"defaultProduct":{"productId":"com.magicv.AirBrush.unlockLifetime","promotionalOfferId":""},"isSubscription":false,"productType":"Lifetime"}],"startTime":1599989220,"title":""}]},"message":"success"}
+
+$done({body: JSON.stringify(obj)});
+
