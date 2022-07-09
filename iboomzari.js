ca50935025b06c8b9c292cb344c6b5303044601a
elzari <86085151+elzari@users.noreply.github.com>
2022-07-09 23:41:41 +0200

    Add files via upload

---
+++ iboomzari.js
@@ -0,0 +1,128 @@
+let obj = JSON.parse($response.body);
+obj ={
+
+  "status" : "0",
+  "receipt-data" : {
+    "environment" : "Production",
+    "receipt" : {
+      "receipt_type" : "Production",
+      "app_item_id" : 1065511007,
+      "receipt_creation_date" : "2022-07-09 21:06:08 Etc/GMT",
+      "bundle_id" : "com.globaldelight.iBoom",
+      "original_purchase_date" : "2020-11-29 03:27:17 Etc/GMT",
+      "in_app" : [
+        {
+          "quantity" : "1",
+          "purchase_date_ms" : "1634945769000",
+          "expires_date" : "2021-10-29 23:36:09 Etc/GMT",
+          "expires_date_pst" : "2021-10-29 16:36:09 America/Los_Angeles",
+          "is_in_intro_offer_period" : "false",
+          "transaction_id" : "300000953582337",
+          "is_trial_period" : "true",
+          "original_transaction_id" : "300000953582337",
+          "purchase_date" : "2021-10-22 23:36:09 Etc/GMT",
+          "product_id" : "com.globaldelight.iBoom.7DayFree1YearIntroPack",
+          "original_purchase_date_pst" : "2021-10-22 16:36:09 America/Los_Angeles",
+          "in_app_ownership_type" : "PURCHASED",
+          "original_purchase_date_ms" : "1634945769000",
+          "web_order_line_item_id" : "300000414281583",
+          "expires_date_ms" : "1635550569000",
+          "purchase_date_pst" : "2021-10-22 16:36:09 America/Los_Angeles",
+          "original_purchase_date" : "2021-10-22 23:36:09 Etc/GMT"
+        },
+        {
+          "quantity" : "1",
+          "purchase_date_ms" : "1653518448000",
+          "expires_date" : "2029-11-25 23:40:48 Etc/GMT",
+          "expires_date_pst" : "2029-11-25 15:40:48 America/Los_Angeles",
+          "is_in_intro_offer_period" : "true",
+          "transaction_id" : "300001124810975",
+          "is_trial_period" : "false",
+          "original_transaction_id" : "300001124810975",
+          "purchase_date" : "2022-05-25 22:40:48 Etc/GMT",
+          "product_id" : "com.globaldelight.iBoom.LifetimeDiscountPack",
+          "original_purchase_date_pst" : "2022-05-25 15:40:51 America/Los_Angeles",
+          "in_app_ownership_type" : "PURCHASED",
+          "original_purchase_date_ms" : "1653518451000",
+          "web_order_line_item_id" : "300000503790486",
+          "expires_date_ms" : "1587096656000",
+          "purchase_date_pst" : "2022-05-25 15:40:48 America/Los_Angeles",
+          "original_purchase_date" : "2022-05-25 22:40:51 Etc/GMT"
+        }
+      ],
+      "adam_id" : 1065511007,
+      "receipt_creation_date_pst" : "2022-07-09 14:06:08 America/Los_Angeles",
+      "request_date" : "2022-07-09 21:06:09 Etc/GMT",
+      "request_date_pst" : "2022-07-09 14:06:09 America/Los_Angeles",
+      "version_external_identifier" : 845913337,
+      "request_date_ms" : "1657400769988",
+      "original_purchase_date_pst" : "2020-11-28 19:27:17 America/Los_Angeles",
+      "application_version" : "2.6.40001",
+      "original_purchase_date_ms" : "1606620437000",
+      "receipt_creation_date_ms" : "1657400768000",
+      "original_application_version" : "2.2.60003",
+      "download_id" : 70074101189797
+    },
+    "status" : 0,
+    "latest_receipt_info" : [
+      {
+        "quantity" : "1",
+        "purchase_date_ms" : "1653518448000",
+        "expires_date" : "2029-11-25 23:40:48 Etc/GMT",
+        "expires_date_pst" : "2029-11-25 15:40:48 America/Los_Angeles",
+        "is_in_intro_offer_period" : "true",
+        "transaction_id" : "300001124810975",
+        "is_trial_period" : "false",
+        "original_transaction_id" : "300001124810975",
+        "purchase_date" : "2022-05-25 22:40:48 Etc/GMT",
+        "product_id" : "com.globaldelight.iBoom.LifetimeDiscountPack",
+        "original_purchase_date_pst" : "2022-05-25 15:40:51 America/Los_Angeles",
+        "in_app_ownership_type" : "PURCHASED",
+        "subscription_group_identifier" : "20461757",
+        "original_purchase_date_ms" : "1653518451000",
+        "web_order_line_item_id" : "300000503790486",
+        "expires_date_ms" : "1587096656000",
+        "purchase_date_pst" : "2022-05-25 15:40:48 America/Los_Angeles",
+        "original_purchase_date" : "2022-05-25 22:40:51 Etc/GMT"
+      },
+      {
+        "quantity" : "1",
+        "purchase_date_ms" : "1634945769000",
+        "expires_date" : "2021-10-29 23:36:09 Etc/GMT",
+        "expires_date_pst" : "2021-10-29 16:36:09 America/Los_Angeles",
+        "is_in_intro_offer_period" : "false",
+        "transaction_id" : "300000953582337",
+        "is_trial_period" : "true",
+        "original_transaction_id" : "300000953582337",
+        "purchase_date" : "2021-10-22 23:36:09 Etc/GMT",
+        "product_id" : "com.globaldelight.iBoom.7DayFree1YearIntroPack",
+        "original_purchase_date_pst" : "2021-10-22 16:36:09 America/Los_Angeles",
+        "in_app_ownership_type" : "PURCHASED",
+        "subscription_group_identifier" : "20461753",
+        "original_purchase_date_ms" : "1634945769000",
+        "web_order_line_item_id" : "300000414281583",
+        "expires_date_ms" : "1635550569000",
+        "purchase_date_pst" : "2021-10-22 16:36:09 America/Los_Angeles",
+        "original_purchase_date" : "2021-10-22 23:36:09 Etc/GMT"
+      }
+    ],
+    "pending_renewal_info" : [
+      {
+        "expiration_intent" : "1",
+        "product_id" : "com.globaldelight.iBoom.7DayFree1YearIntroPack",
+        "is_in_billing_retry_period" : "0",
+        "auto_renew_product_id" : "com.globaldelight.iBoom.7DayFree1YearIntroPack",
+        "original_transaction_id" : "300000953582337",
+        "auto_renew_status" : "0"
+      },
+      {
+        "product_id" : "com.globaldelight.iBoom.LifetimeDiscountPack",
+        "original_transaction_id" : "300001124810975",
+        "auto_renew_product_id" : "com.globaldelight.iBoom.LifetimeDiscountPack.",
+        "auto_renew_status" : "0"
+      }
+    ]
+  }
+}
+
+$done({body: JSON.stringify(obj)})
