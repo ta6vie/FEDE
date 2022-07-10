bb7b3b81bc3482a7b42a6e9c820c1f2a2c9e0ed1
elzari <86085151+elzari@users.noreply.github.com>
2022-07-10 23:42:46 +0200

    Update faceappmadrugada1.js

--- faceappmadrugada1.js
+++ faceappmadrugada1.js
@@ -1,8 +1,12 @@
 var obj = $response.body; 
 obj = {
  
-  "product_ids": ["pro_year"],
-  "receipt_creation_timestamp": 1657417405
+ {
+  "product_ids" : [
+    "pro_month",
+    "pro2"
+  ],
+  "receipt_creation_timestamp" : 1625666766
 }
 
 $done({body});
