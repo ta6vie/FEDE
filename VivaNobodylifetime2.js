e3f720df13b10bb438b6cfed45a58916b1992ff2
elzari <86085151+elzari@users.noreply.github.com>
2022-07-11 08:02:56 +0200

    Add files via upload

---
+++ VivaNobodylifetime2.js
@@ -0,0 +1,23 @@
+var obj = JSON.parse($response.body);
+obj = {
+  "autoRenewProductId": "premium.vip.lifetime",
+  "iosDeviceProductVo": {
+    "premiumVipWeekly": 3,
+    "premiumGoldMonthly": 3,
+    "premiumPlatinumMonthly": 3,
+    "premiumGoldYearly": 3,
+    "premiumPlatinumYearly": 2,
+    "premiumPlatinumHalfYearly": 3,
+    "premiumVipYearly": 3
+  },
+  "isTrialPeriod": true,
+  "endTime": 4081109070000,
+  "platform": 2,
+  "vipType": "premium.vip.lifetime",
+  "duidDgest": "DIzjfUIvln",
+  "autoRenewStatus": 1,
+  "startTime": 1556241871000,
+  "systemDate": 1556965441014
+};
+
+$done({body: JSON.stringify(obj)});
