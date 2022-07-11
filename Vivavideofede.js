a9ed6e738ec8e9a330b7f7257638f0301b6ebfa9
elzari <86085151+elzari@users.noreply.github.com>
2022-07-11 05:22:31 +0200

    Update Vivavideofede.js

--- Vivavideofede.js
+++ Vivavideofede.js
@@ -1,23 +1,50 @@
 var obj = JSON.parse($response.body);
-obj = {
-  "autoRenewProductId": "premium_platinum_yearly",
-  "iosDeviceProductVo": {
-    "premiumVipWeekly": 3,
-    "premiumGoldMonthly": 3,
-    "premiumPlatinumMonthly": 3,
-    "premiumGoldYearly": 3,
-    "premiumPlatinumYearly": 2,
-    "premiumPlatinumHalfYearly": 3,
-    "premiumVipYearly": 3
+obj = 
+
+{
+  "autoRenewStatus" : 1,
+  "originalTransactionId" : "300001040354135",
+  "duidDgest" : "DIzjfUIvln",
+  "hasIntroOfferProds" : [
+
+  ],
+  "iosDeviceProductVo" : {
+    "newVipMonthly" : 3,
+    "premiumVipWeekly" : 3,
+    "premiumPlatinumQuarterly" : 3,
+    "premiumGoldMonthly" : 3,
+    "premiumPlatinumMonthly" : 3,
+    "newVipYearly" : 3,
+    "premiumVipYearly" : 3,
+    "newVipWeekly" : 3,
+    "nonOrganicVipMonthly" : 3,
+    "premiumPlatinumHalfYearly" : 3,
+    "nonOrganicVipWeekly" : 3,
+    "premiumGoldYearly" : 3,
+    "nonOrganicVipYearly" : 3,
+    "premiumPlatinumYearly" : 2
   },
-  "isTrialPeriod": true,
-  "endTime": 4081109070000,
-  "platform": 2,
-  "vipType": "premium_platinum_yearly",
-  "duidDgest": "DIIe86X35",
-  "autoRenewStatus": 1,
-  "startTime": 1556241871000,
-  "systemDate": 1556965441014
-};
+  "platform" : 3,
+  "endTime" : 4073275762000,
+  "systemDate" : 1657508792501,
+  "productList" : [
+    {
+      "isRenew" : true,
+      "vipType" : "premium_gold_yearly"
+    },
+    {
+      "isRenew" : true,
+      "vipType" : "premium_gold_yearly"
+    }
+  ],
+  "hasFreedTrialProds" : [
+    "premium_gold_yearly"
+  ],
+  "isTrialPeriod" : false,
+  "transactionId" : "300001162730793",
+  "startTime" : 1657441810000,
+  "vipType" : "premium_gold_yearly",
+  "sign" : "d61e1aa80648c37ed9cc02178ec19845"
+}
 
 $done({body: JSON.stringify(obj)});
