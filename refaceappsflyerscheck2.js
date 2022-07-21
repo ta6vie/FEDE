2db4eb15079a262e742b727f1a4e586a1a610696
elzari <86085151+elzari@users.noreply.github.com>
2022-07-21 02:50:12 +0200

    Add files via upload

---
+++ refaceappsflyerscheck2.js
@@ -0,0 +1,21 @@
+var obj = JSON.parse($response.body);
+
+obj = 
+
+{
+  "com.neocortext.doublicatapp.subscription.bro.weekly" : {
+    "result" : true,
+    "failure_data" : {
+      "status" : 0,
+      "is_retryable" : false
+    }
+  },
+  "com.neocortext.doublicatapp.subscription.promos.annual.50off" : {
+    "result" : true,
+    "failure_data" : {
+      "status" : 0,
+      "is_retryable" : false
+    }
+  }
+}
+$done({body: JSON.stringify(obj)});
