6215b6223fecc4800619498919922008051fc9bb
elzari <86085151+elzari@users.noreply.github.com>
2022-07-25 08:05:34 +0200

    Add files via upload

---
+++ refacenewrevenuecatreceiptsbeta3.js
@@ -0,0 +1,51 @@
+var obj = JSON.parse($response.body);
+
+obj = 
+
+{
+  "request_date_ms" : 1658728148334,
+  "request_date" : "2022-07-25T05:49:08Z",
+  "subscriber" : {
+    "non_subscriptions" : {
+
+    },
+    "first_seen" : "2022-04-14T21:54:46Z",
+    "original_application_version" : "2010",
+    "other_purchases" : {
+
+    },
+    "management_url" : null,
+    "subscriptions" : {
+      "com.neocortext.doublicatapp.subscription.bro.weekly" : {
+        "is_sandbox" : false,
+        "ownership_type" : "PURCHASED",
+        "billing_issues_detected_at" : null,
+        "period_type" : "normal",
+        "expires_date" : "2023-07-20T20:33:52Z",
+        "unsubscribe_detected_at" : "2022-07-17T07:54:50Z",
+        "original_purchase_date" : "2022-07-13T20:33:55Z",
+        "purchase_date" : "2022-07-13T20:33:52Z",
+        "store" : "app_store"
+      },
+      "com.neocortext.doublicatapp.subscription.promos.annual.50off" : {
+        "is_sandbox" : false,
+        "ownership_type" : "PURCHASED",
+        "billing_issues_detected_at" : null,
+        "period_type" : "normal",
+        "expires_date" : "2023-07-13T06:16:18Z",
+        "unsubscribe_detected_at" : "2022-07-10T06:31:59Z",
+        "original_purchase_date" : "2022-07-10T06:16:19Z",
+        "purchase_date" : "2022-07-10T06:16:18Z",
+        "store" : "app_store"
+      }
+    },
+    "entitlements" : {
+
+    },
+    "original_purchase_date" : "2021-05-07T08:17:44Z",
+    "original_app_user_id" : "f2e303ff-8e22-48c1-b1c0-632a97b53d93",
+    "last_seen" : "2022-07-25T05:49:03Z"
+  }
+}
+
+$done({body: JSON.stringify(obj)});
