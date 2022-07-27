a47be910b36f66b67da82b4f9874e4da5739d686
elzari <86085151+elzari@users.noreply.github.com>
2022-07-27 07:40:43 +0200

    Add files via upload

---
+++ sleepcycleusersubscription.js
@@ -0,0 +1,163 @@
+var obj = JSON.parse($response.body);
+
+obj = 
+
+{
+  "user" : {
+    "created_date" : 1633467777,
+    "email" : "fedemf38@icloud.com",
+    "display_name" : null,
+    "is_temporary" : false,
+    "uid" : "VTo1NDQ3MzM2ODY1NjkzNjk2",
+    "user_id" : "ahFtZGxhYnMtc2xlZXBjeWNsZXIRCxIEVXNlchiAgOiWr8rWCQw"
+  },
+  "subscription" : {
+    "product_id" : "early_adopter_premium_subscription_a_1y",
+    "transaction_id" : "1000000919268018",
+    "campaign" : null,
+    "features_rich" : [
+      {
+        "source" : "subscription",
+        "name" : "online-backup",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "smart-alarm",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "sleep-aid",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "analysis",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "snore",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "sleep-notes",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "premium-sounds",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "user-sounds",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "wake-up-mood",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "heart-rate",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "weather",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "health-kit",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "google-fit",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "statistics",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "philips-hue",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "home-kit",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "weekly-report",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "auto-sleep-tracking",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      },
+      {
+        "source" : "subscription",
+        "name" : "sleep-training",
+        "source_value" : "early-adopter-online-backup",
+        "expire_date" : 4088803483
+      }
+    ],
+    "campaigns" : [
+
+    ],
+    "package_id" : "early-adopter-online-backup",
+    "features" : [
+      "online-backup",
+      "smart-alarm",
+      "sleep-aid",
+      "analysis",
+      "snore",
+      "sleep-notes",
+      "premium-sounds",
+      "user-sounds",
+      "wake-up-mood",
+      "heart-rate",
+      "weather",
+      "health-kit",
+      "google-fit",
+      "statistics",
+      "philips-hue",
+      "home-kit",
+      "weekly-report",
+      "auto-sleep-tracking",
+      "sleep-training"
+    ],
+    "trial" : null,
+    "expire_date" : 4088803483
+  }
+}
+$done({body: JSON.stringify(obj)});
