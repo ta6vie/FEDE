c6dc13bef1d45824a52d77433844c67952726c45
elzari <86085151+elzari@users.noreply.github.com>
2022-07-31 03:49:37 +0200

    Delete myradar.js

--- myradar.js
+++
@@ -1,9 +0,0 @@
-var obj = JSON.parse($response.body);
-
-obj = 
-
-{
-  "subscriptions_url": "https://raw.githubusercontent.com/elzari/FEDE/main/myradarurl",
-  "status": "active"
-}
-$done({body: JSON.stringify(obj)});
