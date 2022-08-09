7168f2c41723adb807e71815bb126fc0b86f14ae
elzari <86085151+elzari@users.noreply.github.com>
2022-08-10 01:32:12 +0200

    Add files via upload

---
+++ fimusgoogle.js
@@ -0,0 +1,357 @@
+var obj = JSON.parse($response.body);
+
+obj = 
+
+{
+  "films" : [
+    {
+      "goodId" : "fimoAPX400",
+      "pay" : "sync",
+      "goodPrice" : 1,
+      "time" : "2022-06-13 10:52:30",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "APX 400"
+    },
+    {
+      "goodId" : "fimoAesthetic400",
+      "pay" : "sync",
+      "goodPrice" : 0,
+      "time" : "2022-06-05 10:38:38",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "Aesthetic 400"
+    },
+    {
+      "goodId" : "fimoBeauty400",
+      "pay" : "sync",
+      "goodPrice" : 2,
+      "time" : "2022-06-13 10:52:30",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "Beauty 400"
+    },
+    {
+      "goodId" : "fimoBubble400",
+      "pay" : "apple",
+      "goodPrice" : 1,
+      "time" : "2022-06-05 11:21:10",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "Bubble 400"
+    },
+    {
+      "goodId" : "fimoBusiness400",
+      "pay" : "sync",
+      "goodPrice" : 0,
+      "time" : "2022-06-05 10:38:38",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "Business 400"
+    },
+    {
+      "goodId" : "fimoCenturia100",
+      "pay" : "sync",
+      "goodPrice" : 2,
+      "time" : "2022-06-13 10:52:30",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "Centuria 100"
+    },
+    {
+      "goodId" : "fimoDE200",
+      "pay" : "apple",
+      "goodPrice" : 2,
+      "time" : "2022-06-05 11:20:51",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "DE 200"
+    },
+    {
+      "goodId" : "fimoEK80",
+      "pay" : "sync",
+      "goodPrice" : 0,
+      "time" : "2022-06-05 10:38:38",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "EK 80"
+    },
+    {
+      "goodId" : "fimoEPR64",
+      "pay" : "apple",
+      "goodPrice" : 1,
+      "time" : "2022-06-05 11:20:27",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "EPR 64"
+    },
+    {
+      "goodId" : "fimoEternaV500",
+      "pay" : "sync",
+      "goodPrice" : 1,
+      "time" : "2022-06-13 10:52:30",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "ETERNA V500"
+    },
+    {
+      "goodId" : "fimoGOLD200",
+      "pay" : "sync",
+      "goodPrice" : 1,
+      "time" : "2022-06-13 10:52:30",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "GOLD 200"
+    },
+    {
+      "goodId" : "fimoHK200",
+      "pay" : "apple",
+      "goodPrice" : 1,
+      "time" : "2022-06-05 11:21:25",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "HK 200"
+    },
+    {
+      "goodId" : "fimoIFDelta100",
+      "pay" : "sync",
+      "goodPrice" : 2,
+      "time" : "2022-06-13 10:52:30",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "Delta 100"
+    },
+    {
+      "goodId" : "fimoIFPAN100",
+      "pay" : "sync",
+      "goodPrice" : 0,
+      "time" : "2022-06-05 10:38:38",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "PAN 100"
+    },
+    {
+      "goodId" : "fimoIMAX5207",
+      "pay" : "sync",
+      "goodPrice" : 2,
+      "time" : "2022-06-13 10:52:30",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "IMAX 5207"
+    },
+    {
+      "goodId" : "fimoJoey100T",
+      "pay" : "apple",
+      "goodPrice" : 1,
+      "time" : "2022-06-05 11:20:38",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "Joey 100T"
+    },
+    {
+      "goodId" : "fimoKDchrome64",
+      "pay" : "apple",
+      "goodPrice" : 2,
+      "time" : "2022-06-05 11:19:56",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "KDchrome 64"
+    },
+    {
+      "goodId" : "fimoLMcolor100",
+      "pay" : "sync",
+      "goodPrice" : 0,
+      "time" : "2022-06-05 10:38:38",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "LM Color 100"
+    },
+    {
+      "goodId" : "fimoMorandi200",
+      "pay" : "apple",
+      "goodPrice" : 1,
+      "time" : "2022-06-05 11:19:45",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "Morandi 200"
+    },
+    {
+      "goodId" : "fimoNatura1600",
+      "pay" : "apple",
+      "goodPrice" : 1,
+      "time" : "2022-06-05 11:19:24",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "Natura 1600"
+    },
+    {
+      "goodId" : "fimoNewYear2020",
+      "pay" : "sync",
+      "goodPrice" : 2,
+      "time" : "2022-06-13 10:52:30",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "New Year 2020"
+    },
+    {
+      "goodId" : "fimoNewYear2021",
+      "pay" : "sync",
+      "goodPrice" : 2,
+      "time" : "2022-06-13 10:52:30",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "ERA 2021"
+    },
+    {
+      "goodId" : "fimoPS98",
+      "pay" : "sync",
+      "goodPrice" : 0,
+      "time" : "2022-06-13 10:52:30",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "PS 98"
+    },
+    {
+      "goodId" : "fimoPortra160NC",
+      "pay" : "apple",
+      "goodPrice" : 2,
+      "time" : "2022-06-05 11:19:35",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "Portra 160NC"
+    },
+    {
+      "goodId" : "fimoPortra160VC",
+      "pay" : "sync",
+      "goodPrice" : 2,
+      "time" : "2022-06-13 10:52:30",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "Portra 160VC"
+    },
+    {
+      "goodId" : "fimoPortra400",
+      "pay" : "sync",
+      "goodPrice" : 2,
+      "time" : "2022-06-13 10:52:30",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "Portra 400"
+    },
+    {
+      "goodId" : "fimoRDP3",
+      "pay" : "sync",
+      "goodPrice" : 2,
+      "time" : "2022-06-13 10:52:30",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "RDP 3"
+    },
+    {
+      "goodId" : "fimoSuperHR100",
+      "pay" : "apple",
+      "goodPrice" : 1,
+      "time" : "2022-06-05 11:19:13",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "Super HR 100"
+    },
+    {
+      "goodId" : "fimoTSE50",
+      "pay" : "sync",
+      "goodPrice" : 2,
+      "time" : "2022-06-13 10:52:30",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "TS-E 50"
+    },
+    {
+      "goodId" : "fimoTokyo500",
+      "pay" : "apple",
+      "goodPrice" : 2,
+      "time" : "2022-06-05 11:20:15",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "Tokyo 500"
+    },
+    {
+      "goodId" : "fimoTriX400",
+      "pay" : "apple",
+      "goodPrice" : 1,
+      "time" : "2022-06-05 11:19:00",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "Tri-X 400"
+    },
+    {
+      "goodId" : "fimoUnicolor125",
+      "pay" : "sync",
+      "goodPrice" : 2,
+      "time" : "2022-06-13 10:52:30",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "Unicolor 125"
+    },
+    {
+      "goodId" : "fimoV3500T",
+      "pay" : "sync",
+      "goodPrice" : 2,
+      "time" : "2022-06-13 10:52:30",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "V3 500T"
+    },
+    {
+      "goodId" : "fimoXMAS25T",
+      "pay" : "sync",
+      "goodPrice" : 2,
+      "time" : "2022-06-13 10:52:30",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "X-Mas 25T"
+    },
+    {
+      "goodId" : "fimoXRed50",
+      "pay" : "apple",
+      "goodPrice" : 1,
+      "time" : "2022-06-05 11:20:06",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "X-Red 50"
+    },
+    {
+      "goodId" : "fimoYummy100",
+      "pay" : "apple",
+      "goodPrice" : 1,
+      "time" : "2022-06-05 11:21:43",
+      "tagName" : "",
+      "photo" : 0,
+      "goodName" : "Yummy 100"
+    }
+  ],
+  "subscribe" : {
+    "valid" : true,
+    "forever" : 1,
+    "endTime" : 0
+  },
+  "filmsCommonOrder" : [
+
+  ],
+  "user" : {
+    "id" : 3287091,
+    "uuid" : "111818679320279841899",
+    "phone" : "",
+    "sex" : null,
+    "time" : "2022-05-31 09:46:27",
+    "wx" : null,
+    "location" : "",
+    "icon" : "https://lh3.googleusercontent.com/a-/AOh14GjluBE9zNpOGj389dLTeMEoJDacEnxE_bsZRMMASg=s96-c",
+    "orgin" : "google",
+    "platform" : 1,
+    "sync" : 1,
+    "email" : "fedemf35@gmail.com",
+    "name" : "ZaRi Zari",
+    "status" : 1
+  }
+}
+$done({body: JSON.stringify(obj)});
