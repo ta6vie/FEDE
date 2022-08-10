fb2a4b0c1616b13d27ca65764d61474a4c93eec6
elzari <86085151+elzari@users.noreply.github.com>
2022-08-10 02:11:45 +0200

    Add files via upload

---
+++ Pdfexpert3.js
@@ -0,0 +1,34 @@
+var obj = JSON.parse($response.body);
+
+obj = 
+
+{
+  "originalTransactionId" : 300001036970978,
+  "inAppStates" : [
+    {
+      "originalTransactionId" : 300001036970978,
+      "productId" : "com.readdle.PDFExpert5.subscription.year50",
+      "subscriptionState" : "active",
+      "productName" : "subscription",
+      "subscriptionExpirationDate" : "05:56 12/05/2099",
+      "isEligibleForIntroPeriod" : false,
+      "subscriptionAutoRenewStatus" : "autoRenewOff",
+      "type" : "subscription",
+      "isInGracePeriod" : false,
+      "isInBillingRetryPeriod" : false,
+      "entitlements" : [
+        "ios.pe.subscription.pdf-features"
+      ]
+    }
+  ],
+  "chargingPlatform" : "iOS AppStore",
+  "receiptStatus" : "ok",
+  "bundleId" : "com.readdle.PDFExpert5",
+  "receiptId" : 1588900570000,
+  "statisticsInfo" : {
+    "events" : [
+
+    ]
+  }
+}
+$done({body: JSON.stringify(obj)});
